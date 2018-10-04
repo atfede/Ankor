a4064c58e6e56337b0ddce3122a7f3b095a4ff0a
Federico <atfede@gmail.com>
2018-10-04 17:01:59 -0300

    pie chart completed

--- src/pages/piechart/piechart.module.ts
+++ src/pages/piechart/piechart.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PiechartPage } from './piechart';
 
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
+
 @NgModule({
   declarations: [
     PiechartPage,
   ],
   imports: [
     IonicPageModule.forChild(PiechartPage),
+    RoundProgressModule
   ],
 })
+
 export class PiechartPageModule {}
